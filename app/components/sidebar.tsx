2c338fae935811baaecb190c9cc1fce6cf2d1e5f
FelipeSantos <felipepegost@gmail.com>
2024-09-11 11:58:16 -0300

    ajustes atualizando layout

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -17,10 +17,10 @@ export function SideBar() {
         </nav>
 
        <nav className="mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-2">
-              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Playlist 1 </a>
-              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Playlist 2 </a>
-              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Playlist 3 </a>
-              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Playlist 4 </a>
+              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Queen Mix</a>
+              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> This is: Pavarotti</a>
+              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Greatest Hits (Remaster) </a>
+              <a href="" className="text-sm text-zinc-400 hover:text-zinc-100"> Rock Forever </a>
 
        </nav>
       </aside>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, Laptop2, Maximize2, Volume, LayoutList} from "lucide-react"
+
+import DarkSideOfTheMoon from '../app/img/Darksideofthemoon.jpg'
+import ThisIsPavarotti from '../app/img/Pavarotti.png'
+import QueenMix from '../app/img/Queen.jpg'
+import ThisIsKanaboon from '../app/img/Kanaboon.jpg'
+import RockForEver from '../app/img/RockForEver.jpg'
+import BohemianRhapsody from '../app/img/bohemianRhapsody.jpg'
+import HardRock from '../app/img/hardrock.jpg'
+import HotelCalifornia from '../app/img/hotelCalifornia.jpg'
+
+
 import Image from "next/image"
 import { SideBar } from "./components/sidebar"
 import { Footer } from "./components/footer"
+import { Header } from "./components/header"
 
 export default function Home() {
 
@@ -11,45 +23,41 @@ return (
       <div className="flex flex-1">
         <SideBar/>
         <main className="flex-1 p-6">
-              <div className="flex items-center gap-4">
-                  <button className="rounded-full bg-black/40 p-1"><ChevronLeft/></button>
-                  <button className="rounded-full bg-black/40 p-1"><ChevronRight/></button>
-              </div>
-
+              <Header/>
   	          <h1 className="font-bold text-3xl mt-10">Good Afternoon</h1>
 
               <div className="grid grid-cols-3 gap-6 mt-4">
                   <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
-                    <Image src="/Darksideofthemoon.jpg" alt="" width={104} height={104}/>
+                    <Image src={DarkSideOfTheMoon} alt="" width={104} height={104}/>
                     <strong>Darkside of the Moon</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                       <Play/>
                     </button>
                   </a>
                   <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-                    <Image src="/Darksideofthemoon.jpg" alt="" width={104} height={104}/>
-                    <strong>Darkside of the Moon</strong>
+                    <Image src={ThisIsPavarotti} alt="" width={104} height={104}/>
+                    <strong>This is: Pavarotti</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                       <Play/>
                     </button>
                   </a>
                   <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-                    <Image src="/Darksideofthemoon.jpg" alt="" width={104} height={104}/>
-                    <strong>Darkside of the Moon</strong>
+                    <Image src={QueenMix} alt="" width={104} height={104}/>
+                    <strong>Queen Mix</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                       <Play/>
                     </button>
                   </a>
                   <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-                    <Image src="/Darksideofthemoon.jpg" alt="" width={104} height={104}/>
-                    <strong>Darkside of the Moon</strong>
+                    <Image src={ThisIsKanaboon} alt="" width={104} height={104}/>
+                    <strong>This is Kana-Boon</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                       <Play/>
                     </button>
                   </a>
                   <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-                    <Image src="/Darksideofthemoon.jpg" alt="" width={104} height={104}/>
-                    <strong>Darkside of the Moon</strong>
+                    <Image src={RockForEver} alt="" width={104} height={104}/>
+                    <strong>Rock Forever </strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                       <Play/>
                     </button>
@@ -60,44 +68,44 @@ return (
 
               <div className="grid grid-cols-8 gap-4 mt-4">
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
+                  <Image src={QueenMix} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">Quem Mix </strong>
+                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                </a>
+                <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                  <Image src={BohemianRhapsody} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">Bohemian Rhapsody</strong>
                   <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
                   <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
+                  <strong className="font-semibold">Darkside of the moon </strong>
                   <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                  <Image src={ThisIsKanaboon} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">This is Kana-boon </strong>
+                  <span className="text-sm text-zinc-400">This is Kana-boon, Queen Mix and more</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                  <Image src={HotelCalifornia} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">Hotel California (2013 Remaster) </strong>
+                  <span className="text-sm text-zinc-400">Eagles and more</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                  <Image src={ThisIsPavarotti} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">This is Pavarotti </strong>
+                  <span className="text-sm text-zinc-400">This is Pavarotti, Classical Hits and more</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                  <Image src={RockForEver} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">Rock Mix </strong>
+                  <span className="text-sm text-zinc-400">Rock Forever, Aerosmith, Queen and more.</span>
                 </a>
                 <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
-                </a>
-                <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-                  <Image src="/Darksideofthemoon.jpg" className="w-full" alt="Darkside of the moon." width={120} height={120}/>
-                  <strong className="font-semibold">Daily Mix 1 </strong>
-                  <span className="text-sm text-zinc-400">Wallowsm, Coin, girl in red and more</span>
+                  <Image src={HardRock} className="w-full" alt="Darkside of the moon." width={120} height={120}/>
+                  <strong className="font-semibold">Mix Hard Rock </strong>
+                  <span className="text-sm text-zinc-400">Darkside of The Moon, Dragonforce and more</span>
                 </a>
               </div>
 
